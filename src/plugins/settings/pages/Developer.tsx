d32598a41b842852b4db7e8c65406bd60a033b4e
PalmDevs <git@palmdevs.me>
2024-11-16 03:37:23 +0700

    feat(plugins/settings): add eval tool to Developer page

--- src/plugins/settings/pages/Developer.tsx
+++ src/plugins/settings/pages/Developer.tsx
@@ -1,13 +1,58 @@
-import { NavigationNative } from '@revenge-mod/modules/common'
-import { Stack, TableRow, TableRowGroup, TableRowIcon } from '@revenge-mod/modules/common/components'
+import { alerts, NavigationNative } from '@revenge-mod/modules/common'
+import {
+    AlertActionButton,
+    AlertModal,
+    Stack,
+    TableRow,
+    TableRowGroup,
+    TableRowIcon,
+    TextArea,
+} from '@revenge-mod/modules/common/components'
 import { BundleUpdaterManager } from '@revenge-mod/modules/native'
 
 export default function DeveloperSettingsPage() {
     const { assets, modules } = revenge
     const navigation = NavigationNative.useNavigation()
+    const inputRef = React.useRef('')
 
     return (
         <Stack style={{ paddingHorizontal: 16, paddingVertical: 24 }} spacing={16} direction="vertical">
+            <TableRowGroup title="Tools">
+                <TableRow
+                    label="Evaluate JavaScript"
+                    icon={<TableRowIcon source={assets.getIndexByName('PaperIcon')} />}
+                    onPress={() => {
+                        alerts.openAlert(
+                            'revenge.plugins.settings.developer.evaluate',
+                            <AlertModal
+                                title="Evaluate JavaScript"
+                                extraContent={
+                                    <TextArea
+                                        autoFocus
+                                        label="Code"
+                                        size="md"
+                                        placeholder="ReactNative.NativeModules.BundleUpdaterManager.reload()"
+                                        onChange={(v: string) => (inputRef.current = v)}
+                                    />
+                                }
+                                actions={
+                                    <Stack>
+                                        <AlertActionButton
+                                            text="Evaluate"
+                                            variant="primary"
+                                            onPress={() =>
+                                                // biome-ignore lint/security/noGlobalEval: This is intentional
+                                                alert(modules.findProp.lazy('inspect')(globalThis.eval(inputRef.current)))
+                                            }
+                                        />
+                                        <AlertActionButton text="Cancel" variant="secondary" />
+                                    </Stack>
+                                }
+                            />,
+                        )
+                    }}
+                />
+            </TableRowGroup>
             <TableRowGroup title="Navigation">
                 <TableRow
                     label="Test CustomPageRenderer"
