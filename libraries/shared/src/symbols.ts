978c2c24cec6969bd6fa41ee80006bd146e66b26
PalmDevs <git@palmdevs.me>
2024-11-15 23:35:26 +0700

    fix(libraries/shared)!: remove unused `libraryPatcherSymbol`

--- libraries/shared/src/symbols.ts
+++ libraries/shared/src/symbols.ts
@@ -1,10 +1,4 @@
 /**
- * Symbol used to store a library's patcher instance in case it needs to be destroyed
- * @internal
- */
-export const libraryPatcherSymbol = Symbol.for('revenge.shared.libraryPatcher')
-
-/**
  * Symbol used to access internal properties
  * @internal
  */
