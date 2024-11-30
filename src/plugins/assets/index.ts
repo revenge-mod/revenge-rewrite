1fff80c04bca01bd04c0ed205c2d8517d08ed587
PalmDevs <git@palmdevs.me>
2024-12-01 01:37:55 +0700

    feat(assets): add `Revenge.PluginIcon`

--- src/plugins/assets/index.ts
+++ src/plugins/assets/index.ts
@@ -2,13 +2,14 @@ import { registerCustomAsset } from '@revenge-mod/assets'
 import { registerPlugin } from '@revenge-mod/plugins/internals'
 
 import HermesIcon from '../../assets/hermes.webp'
+import PluginIcon from '../../assets/plugin.webp'
 import ReactIcon from '../../assets/react.webp'
 import RevengeIcon from '../../assets/revenge.webp'
 
 registerPlugin(
     {
         name: 'Assets',
-        author: 'The Revenge Team',
+        author: 'Revenge',
         description: "Registers Revenge's assets as React Native assets",
         id: 'revenge.assets',
         version: '1.0.0',
@@ -36,6 +37,14 @@ registerPlugin(
                     type: 'webp',
                 },
                 ReactIcon,
+            )
+
+            registerCustomAsset(
+                {
+                    name: 'Revenge.PluginIcon',
+                    type: 'webp',
+                },
+                PluginIcon,
             )
         },
     },
