348ca5929ea8eb38a4ec44533caa6dae6f563726
PalmDevs <git@palmdevs.me>
2024-12-01 14:45:09 +0700

    feat(ui/components): add `FormSwitch` wrapper

--- libraries/ui/src/components.tsx
+++ libraries/ui/src/components.tsx
@@ -1,8 +1,16 @@
-import { TextInput } from '@revenge-mod/modules/common/components'
+import { TextInput, FormSwitch as _FormSwitch } from '@revenge-mod/modules/common/components'
 import { findProp } from '@revenge-mod/modules/finders'
+
+import { StyleSheet, View } from 'react-native'
 
 import type { DiscordModules } from '@revenge-mod/modules'
 import type { ComponentProps } from 'react'
+
+const styles = StyleSheet.create({
+    disabled: {
+        opacity: 0.5,
+    },
+})
 
 export function SearchInput(props: ComponentProps<DiscordModules.Components.TextInput>) {
     return (
@@ -12,5 +20,16 @@ export function SearchInput(props: ComponentProps<DiscordModules.Components.Text
             placeholder={props.placeholder ?? 'Search'}
             returnKeyType="search"
         />
+    )
+}
+
+/**
+ * A switch component that is styled to match Discord's configurationw
+ */
+export function FormSwitch(props: ComponentProps<DiscordModules.Components.FormSwitch>) {
+    return (
+        <View style={props.disabled ? styles.disabled : undefined}>
+            <_FormSwitch {...props} />
+        </View>
     )
 }
