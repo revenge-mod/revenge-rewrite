7a4973abd193afe673042658a626926afe5b7936
PalmDevs <git@palmdevs.me>
2025-01-03 15:35:00 +0700

    fix(libraries/ui/colors): hint lazy exports as objects

--- libraries/ui/src/colors.ts
+++ libraries/ui/src/colors.ts
@@ -2,8 +2,8 @@ import { tokens } from '@revenge-mod/modules/common'
 import { ThemeStore } from '@revenge-mod/modules/common/stores'
 import { lazyValue } from '@revenge-mod/utils/lazy'
 
-export const SemanticColor = lazyValue(() => tokens.colors) as Record<string, symbol & { __TYPE__: 'Color' }>
-export const RawColor = lazyValue(() => tokens.unsafe_rawColors) as Record<string, string>
+export const SemanticColor = lazyValue(() => tokens.colors, { hint: 'object' }) as Record<string, symbol & { __TYPE__: 'Color' }>
+export const RawColor = lazyValue(() => tokens.unsafe_rawColors, { hint: 'object' }) as Record<string, string>
 
 export function isSemanticColor(key: string): boolean {
     return tokens.internal.isSemanticColor(key)
