a0bc2b5cfd100ad4dd70f9566ea249a747295dbe
nexpid <60316309+nexpid@users.noreply.github.com>
2025-01-03 21:19:28 +0100

    feat: misc

--- src/plugins/developer-settings/index.tsx
+++ src/plugins/developer-settings/index.tsx
@@ -1,7 +1,7 @@
 /// <reference path="./debugger.d.ts" />
 
 import { toasts } from '@revenge-mod/modules/common'
-import { registerPlugin } from '@revenge-mod/plugins/internals'
+import { externalPluginsMetadata, registerPlugin } from '@revenge-mod/plugins/internals'
 import { sleep } from '@revenge-mod/utils/functions'
 
 import AssetBrowserSettingsPage from './pages/AssetBrowser'
@@ -11,9 +11,10 @@ import DeveloperSettingsPage from './pages/Developer'
 import { DebuggerContext, connectToDebugger } from './debugger'
 import { DevToolsEvents, connectToDevTools } from './devtools'
 
-import { BundleUpdaterManager } from '@revenge-mod/modules/native'
+import { BundleUpdaterManager, FileModule } from '@revenge-mod/modules/native'
 import type { PluginContextFor } from '@revenge-mod/plugins'
 import type { FunctionComponent } from 'react'
+import { PluginsDirectoryPath } from '@revenge-mod/shared/paths'
 
 const plugin = registerPlugin<{
     reactDevTools: {
@@ -137,6 +138,15 @@ function setupDebugger({ patcher, cleanup }: PluginContextFor<typeof plugin, 'Af
     )
 
     globalThis.reload = () => BundleUpdaterManager.reload()
+    globalThis.getRidOfPlugin = id => {
+        try {
+            FileModule.removeFile('documents', `${PluginsDirectoryPath}/${id}`)
+            delete externalPluginsMetadata[id]
+            return true
+        } catch {
+            return false
+        }
+    }
     globalThis.patcher = {
         snipe: (object, key, callback) =>
             debuggerCleanups.add(
@@ -163,6 +173,8 @@ function setupDebugger({ patcher, cleanup }: PluginContextFor<typeof plugin, 'Af
             delete globalThis.reload
             // biome-ignore lint/performance/noDelete: This happens once
             delete globalThis.patcher
+            // biome-ignore lint/performance/noDelete: This happens once
+            delete globalThis.getRidOfPlugin
         },
         () => {
             for (const c of debuggerCleanups) c()
