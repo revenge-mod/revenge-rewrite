11410f6bdcda1c6ad363a6b3a746515646d54f27
PalmDevs <git@palmdevs.me>
2025-01-06 00:10:10 +0700

    fix(libraries/shared/paths): add missing export

--- libraries/shared/src/paths.ts
+++ libraries/shared/src/paths.ts
@@ -9,6 +9,7 @@ export const PluginsDirectoryPath = `${BaseDirectory}/plugins`
 export const PluginsStatesFilePath = `${PluginsDirectoryPath}/states.json`
 export const ExternalPluginsMetadataFilePath = `${PluginsDirectoryPath}/externals.json`
 
-export const ExternalPluginManifestFilePath = (id: string) => `${PluginsDirectoryPath}/${id}/manifest.json`
-export const ExternalPluginSourceFilePath = (id: string) => `${PluginsDirectoryPath}/${id}/source.zip`
-export const PluginStoragePath = (id: string) => `${PluginsDirectoryPath}/${id}/storage.json`
+export const PluginDirectoryPath = (id: string) => `${PluginsDirectoryPath}/${id}`
+export const ExternalPluginManifestFilePath = (id: string) => `${PluginDirectoryPath(id)}/manifest.json`
+export const ExternalPluginSourceFilePath = (id: string) => `${PluginDirectoryPath(id)}/source.zip`
+export const PluginStoragePath = (id: string) => `${PluginDirectoryPath(id)}/storage.json`
